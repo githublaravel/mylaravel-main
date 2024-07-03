361175eeb0df5fb4e786fd63bde0f6228834d849
AmirRezaMansoori <mansoriporamirrezaa@gmail.com>
2024-07-04 00:37:07 +0330

    s

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\OtherController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\UserController;
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,24 +19,72 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//R = Reall
 
-Route::get('/home', [OtherController::class, 'home']);
+//Basic
+//r = Reall
+Route::get('/', [OtherController::class, 'home']);
 
 //Route For Blog
 Route::prefix('/blog')->group(function () {
     Route::get('/read/{id}', [BlogController::class, 'show']);
-    Route::get('/create', [BlogController::class, 'create']);
-    Route::post('/create', [BlogController::class, 'R_create']);
-    Route::get('/edit/{id}', [BlogController::class, 'edit']);
-    Route::post('/edit', [BlogController::class, 'Redite']);
-    Route::get('/destory/{id}', [BlogController::class, 'destory']);
+    Route::post('/comment', [CommentController::class, 'create']);
 });
+//End Blog
 
 //Route For User
 Route::prefix('/user')->group(function () {
     Route::get('/profile', [UserController::class, 'show']);
     Route::get('/edit/{id}', [UserController::class, 'edit']);
-    Route::post('/edit', [UserController::class, 'R_edit']);
+    Route::post('/edit', [UserController::class, 'r_edit']);
 });
+//End User
 
+//Route For Question
+Route::prefix('/question')->group(function () {
+    Route::get('/read/{id}', [QuestionController::class, 'show']);
+    Route::get('/create', [QuestionController::class, 'create']);
+    Route::post('/create', [QuestionController::class, 'r_create']);
+    Route::get('/edit/{id}', [QuestionController::class, 'edit']);
+    Route::post('/edit', [QuestionController::class, 'r_edit']);
+    Route::get('/delete', [QuestionController::class, 'destory']);
+    Route::post('/chat', [ChatController::class, 'create']);
+});
+//End Question
+//End basic
+
+//Route For Admins
+//a = admin
+Route::prefix('/admin')->group(function () {
+    Route::prefix('/blog')->group(function () {
+        Route::get('/read/', [BlogController::class, 'table']);
+        Route::get('/create', [BlogController::class, 'a_create']);
+        Route::post('/create', [BlogController::class, 'a_r_create']);
+        Route::get('/edit/{id}', [BlogController::class, 'a_edit']);
+        Route::post('/edit', [BlogController::class, 'a_r_edite']);
+        Route::get('/destory/{id}', [BlogController::class, 'delete']);
+    });
+    Route::prefix('/user')->group(function () {
+        Route::get('/read', [UserController::class, 'table']);
+        Route::get('/edit/{id}', [UserController::class, 'a_edit']);
+        Route::post('/edit', [UserController::class, 'a_r_edit']);
+        Route::get('/delete', [UserController::class, 'delete']);
+    });
+    Route::prefix('/question')->group(function () {
+        Route::get('/read', [QuestionController::class, 'table']);
+        Route::get('/create', [QuestionController::class, 'a_create']);
+        Route::post('/create', [QuestionController::class, 'a_r_create']);
+        Route::get('/edit/{id}', [QuestionController::class, 'a_edit']);
+        Route::post('/edit', [QuestionController::class, 'a_r_edit']);
+        Route::get('/delete', [QuestionController::class, 'delete']);
+    });
+    Route::prefix('comment')->group(function () {
+        Route::get('/read', [CommentController::class, 'table']);
+        Route::get('/accept', [CommentController::class, 'accpet']);
+        Route::get('/decline', [CommentController::class, 'decline']);
+    });
+    Route::prefix('chat')->group(function () {
+        Route::get('/read', [CommentController::class, 'table']);
+        Route::get('/accept', [CommentController::class, 'accpet']);
+        Route::get('/decline', [CommentController::class, 'decline']);
+    });
+});
