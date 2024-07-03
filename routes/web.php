d8a774f5bb8e6ce5a895a671b8f02b5bbcb605fa
AmirRezaMansoori <mansoriporamirrezaa@gmail.com>
2024-07-03 23:29:33 +0330

    s

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\OtherController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +15,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//R = Reall
+
+Route::get('/home', [OtherController::class, 'home']);
+
+//Route For Blog
+Route::prefix('/blog')->group(function () {
+    Route::get('/read/{id}', [BlogController::class, 'show']);
+    Route::get('/create', [BlogController::class, 'create']);
+    Route::post('/create', [BlogController::class, 'R_create']);
+    Route::get('/edit/{id}', [BlogController::class, 'edit']);
+    Route::post('/edit', [BlogController::class, 'Redite']);
+    Route::get('/destory/{id}', [BlogController::class, 'destory']);
+});
+
+//Route For User
+Route::prefix('/user')->group(function () {
+    Route::get('/profile', [UserController::class, 'show']);
+    Route::get('/edit/{id}', [UserController::class, 'edit']);
+    Route::post('/edit', [UserController::class, 'R_edit']);
+});
+
